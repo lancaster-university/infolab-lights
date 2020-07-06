a94a87cf19e4808bbf81ecbe4209893fb86048e0
Ben Simms <ben@bensimms.moe>
2020-07-06 23:40:09 +0100

    Enable compression of responses

--- web/rel/config/config.exs
+++ web/rel/config/config.exs
@@ -5,6 +5,6 @@ port = String.to_integer(System.get_env("PORT") || "4000")
 default_secret_key_base = :crypto.strong_rand_bytes(43) |> Base.encode64
 
 config :infolab_light_games, InfolabLightGamesWeb.Endpoint,
-  http: [port: port],
+  http: [port: port, compress: true],
   url: [host: host, port: port],
   secret_key_base: System.get_env("SECRET_KEY_BASE") || default_secret_key_base
