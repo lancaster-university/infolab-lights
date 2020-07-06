175f157955e44424593d9c6cbefb833d7de3bebe
Ben Simms <ben@bensimms.moe>
2020-07-06 14:21:04 +0100

    Pass host env value

--- web/rel/config/config.exs
+++ web/rel/config/config.exs
@@ -1,9 +1,10 @@
 use Mix.Config
 
+host = System.get_env("HOST") || "localhost"
 port = String.to_integer(System.get_env("PORT") || "4000")
 default_secret_key_base = :crypto.strong_rand_bytes(43) |> Base.encode64
 
 config :infolab_light_games, InfolabLightGamesWeb.Endpoint,
   http: [port: port],
-  url: [host: "localhost", port: port],
+  url: [host: host, port: port],
   secret_key_base: System.get_env("SECRET_KEY_BASE") || default_secret_key_base
