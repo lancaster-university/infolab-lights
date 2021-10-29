bce29e22aaeebf2579989297d98ba74d02530b71
Ben Simms <ben@bensimms.moe>
2021-10-29 12:27:33 +0100

    Fix some things

--- web/config/runtime.exs
+++ web/config/runtime.exs
@@ -1,12 +1,13 @@
 import Config
 
-host = System.get_env("HOST") || "localhost"
+hosts = String.split(System.get_env("HOSTS") || "http://localhost:4000")
 port = String.to_integer(System.get_env("PORT") || "4000")
 default_secret_key_base = :crypto.strong_rand_bytes(65) |> Base.encode64()
 
 config :infolab_light_games, InfolabLightGamesWeb.Endpoint,
   http: [port: port, compress: true],
-  url: [host: host, port: port],
+  url: [host: nil, port: port],
+  check_origin: hosts,
   secret_key_base: System.get_env("SECRET_KEY_BASE") || default_secret_key_base
 
 config :infolab_light_games, InfolabLightGamesWeb.Router,
