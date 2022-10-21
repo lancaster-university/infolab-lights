de138dfe1d8fd509aa070d4bea8dc6577d845930
james1236 <32351696+james1236@users.noreply.github.com>
2022-10-22 00:54:29 +0100

    Fix the lag and make the code way better (#13)

--- web/priv/js_effects/matrix.js
+++ web/priv/js_effects/matrix.js
@@ -1,65 +1,61 @@
-return class MatrixEffect {
+return class MartixEffect {
   
   constructor(display) {
     this.display = display;
-
-    this.letters = [];
     this.frame = 0;
-
-    this.#clear();
-  }
-
-  #clear() {
-    for (let x = 0; x < this.display.width; x++) {
-      for (let y = 0; y < this.display.height; y++) {
-        this.display.setPixel(x, y, [0, 0, 0]);
+    
+    //Create buffer
+    this.buffer = [];
+    for (let y = 0; y < this.display.height; y++) {
+      let line = [];
+      for (let x = 0; x < this.display.width; x++) {
+        line.push([0,0,0]);
       }
+      this.buffer.push(line);
     }
-
-    this.display.flush();
   }
 
+  //Fade out all the pixels in a row by a percentage
+  #fade(row, fadeAmount) {
+    fadeAmount = (1 - fadeAmount);
+    for (let i = 0; i < row.length; i++) {
+      row[i] = [
+        row[i][0] * fadeAmount, 
+        row[i][1] * fadeAmount, 
+        row[i][2] * fadeAmount
+      ];
+    }
+    return row;
+  }
+  
+  //Called 25 times a second
   update() {
-    this.#clear();
+    //Decrease percentage to fade out each new top row (longer trails)
+    let fadeAmount = 0.2 - (this.frame / 12000);
 
+    //Duplicate the top row and fade it out, move everything down
+    this.buffer.pop();
+    let newRow = this.#fade(this.buffer[0], fadeAmount);
+    this.buffer.unshift(JSON.parse(JSON.stringify(newRow))); //Pass by reference = cringe
+
+    //Randomly place green pixels in the top row
     for (let i = 0; i < 100; i++) {
-      if (Math.floor(Math.random() * Math.max((500 - (Math.pow(this.frame, 1)/4)),20)) == 0) {
-        this.letters.push([
-          Math.floor(Math.random() * this.display.width), //Random X
-          0, //Y
-          Math.max(
-            (0.2 - (this.frame / 12000)) *    //Starting amount of delta opacity 
-            1 + ((Math.random() - 0.5)*0), 0
-          ), 
-          [
+      if (Math.floor(Math.random() * Math.max(500 - (this.frame / 4),20)) == 0) { //Increase placement chance each frame
+        let randomX = Math.floor(Math.random() * this.buffer[0].length);
+        this.buffer[0][randomX] = [
             Math.floor(Math.random() * 0),   //Starting color
             Math.floor(Math.random() * 255), 
             Math.floor(Math.random() * 0), 
-          ]
-        ]);
+        ];
       }
     }
 
-    for (let i = 0; i < this.letters.length; i++) {
-      let letter = this.letters[i];
-
-      //Trail
-      let opacity = 1;
-      for (let y = letter[1]; y > 0; y--) {
-        this.display.setPixel(letter[0], y, [Math.round(letter[3][0] * opacity), Math.round(letter[3][1] * opacity), Math.round(letter[3][2] * opacity)]);
-        opacity -= letter[2];
+    //Update display
+    for (let y = 0; y < this.buffer.length; y++) {
+      let row = this.buffer[y];
+      for (let x = 0; x < row.length; x++) {
+        this.display.setPixel(x, y, row[x]);
       }
-
-      //Move down
-      this.letters[i] = [letter[0], letter[1] + 1, letter[2], letter[3]];
-      /*if (this.letters[i][1] >= 1000) {
-        this.letters.splice(i, 1);
-        i--;
-      }*/  
-    }
-
-    while (this.letters.length > 1000) {
-      this.letters.splice(0,1);
     }
 
     this.frame++;
