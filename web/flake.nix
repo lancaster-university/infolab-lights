05aac4e7f9f18d826acdf01a43b6a54b058008dc
Ben Simms <ben@bensimms.moe>
2023-01-30 13:58:31 +0000

    Improve dockerfile (don't take 10 minutes to build)

--- web/flake.nix
+++ web/flake.nix
@@ -3,19 +3,31 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in
-      rec {
-        packages.default = devShells.default;
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+        rustComponents = pkgs.fenix.complete.withComponents [ "cargo" "rustc" ];
+        buildPackages = with pkgs; [
             nodejs
             yarn
+            rustComponents
+            beam.packages.erlangR25.elixir_1_14
+            beam.packages.erlang.rebar3
+            beam.packages.erlangR25.hex
+            erlangR25
+            git
           ];
+      in
+      rec {
+        devShells.default = pkgs.mkShell {
+          buildInputs = buildPackages;
         };
       }
     );
